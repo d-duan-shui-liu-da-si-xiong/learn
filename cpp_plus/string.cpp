d0d2daa7fd4fa5d5d16341d46607105509392973
lin xiao long <linxiaolong@baidu.com>
2020-09-26 12:20:31 +0800

    string test

---
+++ cpp_plus/string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int main()
+{
+	using namespace std;
+	string str1 = "learning cpp primer plus.";
+	char cstr1[] = "learning cpp primer plus.";
+	cout << str1 << sizeof(str1) << endl;
+	cout << strlen(cstr1) << endl;
+	cout << str1.size() << endl;
+	
+	//input a string to string obj
+	string s;
+	cout << "please input your name:";
+	getline(cin, s);
+	cout << "you input is:"
+		 << s << endl;
+
+	return 0;
+}
