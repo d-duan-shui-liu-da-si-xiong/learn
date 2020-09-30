1e54964d59e0367942e416c76047973d6f77c42f
lin xiao long <linxiaolong@baidu.com>
2020-09-30 23:42:51 +0800

    setment fault test

---
+++ cpp_plus/seg_fault.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int add_test_segment()
+{
+    int *p = NULL;
+    // *p = 900;
+     printf("%d\n",  *p);
+
+     return -1;
+}
+
+int main()
+{
+    int i;
+    i = add_test_segment();
+
+    return 0;
+}
