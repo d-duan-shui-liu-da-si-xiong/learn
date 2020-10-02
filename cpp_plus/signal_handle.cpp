986b5d61f34e38e96c7604cc1ecc9084f015a127
lin xiao long <linxiaolong@baidu.com>
2020-10-02 23:23:39 +0800

    add -rdynamic compile switch

--- cpp_plus/signal_handle.cpp
+++ cpp_plus/signal_handle.cpp
@@ -4,18 +4,20 @@
 
 using namespace std;
 
-void printStack()
+void printStack(int s)
 {
     size_t len = 1024;
     size_t size;
     void *func[len];
     char **funcs;
 
+    signal(s, SIG_DFL);
     size = backtrace(func, len);
     funcs = (char **)backtrace_symbols(func, size);
     for (int i = 0; i < size; i++) {
         printf("%d %s\n", i, funcs[i]);
     }
+    free(funcs);
 }
 
 void signalHandler(int sig)
@@ -23,10 +25,11 @@ void signalHandler(int sig)
     cout << "encount an error: ";
     cout << sig << endl;
     cout << "stack: " << endl;
-    printStack();
+    printStack(sig);
 
     exit(sig);
 }
+
 void construct_error()
 {
     int *p = NULL;
@@ -38,6 +41,8 @@ void construct_error2()
     construct_error();
 }
 
+//g++ -rdynamic signal_handle.cpp -o signal_handle
+//rdynamic produce function name
 int main()
 {
     //reg signal handler
